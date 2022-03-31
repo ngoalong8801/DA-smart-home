ae366c2fa7ca9912a84a6934df8371ba4d41f216
Tin Phan <tin.phanbk19@hcmut.edu.vn>
2022-03-31 22:43:50 +0700

    update

--- src/app/controllers/siteController.js
+++ src/app/controllers/siteController.js
@@ -12,28 +12,16 @@ class SiteController {
 		if (req.user) {
 			user = JSON.parse(JSON.stringify(req.user));
 		}
-
-<<<<<<< HEAD
 		res.render("homepage/home", {user: user});
 	}
-=======
-    getHomePage(req, res, next) {
-        var user = {}
-        if(req.user){
-            user = JSON.parse(JSON.stringify(req.user));
-            }
-        res.render('homepage/home', {user: user });
-    }
 
-    requireLoginAPI(req, res, next) {
-        console.log(req.isAuthenticated())
-            if (!req.isAuthenticated()) {
-        return res.send({loggedIn: false});
-    } next()
-    }
-    
-    
->>>>>>> 39fcf5a0bce5c7ac31c437b4aa0ce4332b4fe7ad
+	requireLoginAPI(req, res, next) {
+		console.log(req.isAuthenticated());
+		if (!req.isAuthenticated()) {
+			return res.send({loggedIn: false});
+		}
+		next();
+	}
 }
 
 module.exports = new SiteController();
